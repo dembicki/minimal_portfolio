80ae46ab1b51a970cc345ee18cc250f81442d887
dembicki <nikeid1235@gmail.com>
2021-03-08 20:03:18 +0100

    eslint config

---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  background-color: red;
+`;
+
+export default function SectionWrapper() {
+  return (
+    <Wrapper>
+      <p>test</p>
+    </Wrapper>
+  );
+}
