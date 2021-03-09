0daafce8eaea6f5e1ee2469a9feebd97e3bcca97
dembicki <nikeid1235@gmail.com>
2021-03-09 22:48:09 +0100

    Created experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,43 +4,49 @@ import { Chrono } from "react-chrono";
 import styled from "styled-components";
 
 export default function Experience() {
-  const Wrapper = styled.div``;
+  const Wrapper = styled.div`
+    padding: 0;
+  `;
+
+  const CardWrapper = styled.div``;
+
+  const JobTitle = styled.h2`
+    font-size: 30px;
+    font-weight: bolder;
+  `;
+
+  const CompanyName = styled.h3`
+    font-size: 1.4rem;
+    font-weight: normal;
+  `;
+
+  const Description = styled.ul`
+    font-size: 1.4rem;
+    line-height: 2rem;
+  `;
 
   const items = [
     {
-      cardTitle: "Junior Frontend Developer",
-      cardSubtitle: "LPN PLANT, March 2020 - Present",
-      cardDetailedText: `• Developing dashboard for wireless managing outdoor
-      lights installations using React, Postgres, GraphQL, Ant Design, Nodejs
-      with schedule planning integration and data analytics.
-      • Developing dashboard for IoT data analytics using Cube.js, React, GraphQL, Nodejs
-      • Rebranding existing client’s landing pages
-      • Doing researches for potencial project tech stack based on customer needs
-      • Managing small webdev team for some time.`,
+      jobTitle: "Junior Frontend Developer",
+      companyName: "LPN PLANT, March 2020 - Present",
+      Description: [
+        "Developing dashboard for wireless managing outdoor lights installations using React Postgres, GraphQL, Ant Design, Nodejs with schedule planning integration and data analytics.",
+        "Developing dashboard for IoT data analytics using Cube.js, React, GraphQL, Nodejs",
+        "Rebranding existing client’s landing pages",
+        "Doing researches for potencial project tech stack based on customer needs",
+        "Managing small webdev team for some time.",
+      ],
     },
     {
-      cardTitle: "Graphic Designer",
-      cardSubtitle: "Mayeryn , Jan 2019 - Apr 2020",
-      cardDetailedText: `• Creating full branding
-        • Developing mayeryn.com website
-        • Creating layouts for embbeded systems
-        • Creaing layouts for web and mobile apps
-        • Creating extra designes
-        • Takeing care of ui coherence for web and mobiles apps
-        • Working in SCRUM and KANBAN methodology
-        • Full Remote Work`,
-    },
-    {
-      cardTitle: "Web Designer/ Web Developer",
-      cardSubtitle: "Social Unicorn Creative Agency, June 2019 - March 2020",
-      cardDetailedText: `• Creating websites and shops based on Wordpress, Prestashop
-        with extra custom features
-        • Creating landing pages using HTML, CSS, Vanilla JS
-        • Optimization websites for page speed and seo
-        • Work in small teams
-        • Learning React and Gatbsyjs
-        • Creating graphics designs
-        • Partly Remote Work`,
+      jobTitle: "Junior Frontend Developer",
+      companyName: "LPN PLANT, March 2020 - Present",
+      Description: [
+        "Developing dashboard for wireless managing outdoor lights installations using React Postgres, GraphQL, Ant Design, Nodejs with schedule planning integration and data analytics.",
+        "Developing dashboard for IoT data analytics using Cube.js, React, GraphQL, Nodejs",
+        "Rebranding existing client’s landing pages",
+        "Doing researches for potencial project tech stack based on customer needs",
+        "Managing small webdev team for some time.",
+      ],
     },
   ];
 
@@ -49,18 +55,28 @@ export default function Experience() {
   return (
     <Wrapper>
       <Chrono
-        items={items}
         mode="VERTICAL"
-        scrollable
+        scrollable={{ scrollbar: false }}
         hideControls
-        disableNavOnKey
         theme={{
           primary: "white",
           secondary: "black",
           cardBgColor: "transparent",
-          cardForeColor: "white",
+          cardForeColor: "violet",
         }}
-      />
+      >
+        {items.map((item) => (
+          <CardWrapper>
+            <JobTitle>{item.jobTitle}</JobTitle>
+            <CompanyName>{item.companyName}</CompanyName>
+            <Description>
+              {item.Description.map((d) => (
+                <li>{d}</li>
+              ))}
+            </Description>
+          </CardWrapper>
+        ))}
+      </Chrono>
     </Wrapper>
   );
 }
