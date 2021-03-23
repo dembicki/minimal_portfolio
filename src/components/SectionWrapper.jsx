632195844bb3aacafdfd61416cb4f87645bbcced
dembicki <nikeid1235@gmail.com>
2021-03-23 17:53:49 +0100

    Code cleanup

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function SectionWrapper({ children, title, id, minHeight }) {
-  const Wrapper = styled.section`
-    padding-top: 2rem;
-    max-width: 1200px;
-    min-height: 100vh;
-    min-height: ${minHeight};
-  `;
-  const Heading = styled.h2`
-    font-size: 30px;
-    text-align: left;
-    margin: 1rem 0;
-  `;
-
+export default function SectionWrapper({ children, title, id }) {
   return (
     <Wrapper id={id}>
       <Heading>{`${title}:`}</Heading>
@@ -21,3 +9,14 @@ export default function SectionWrapper({ children, title, id, minHeight }) {
     </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  padding-top: 2rem;
+  max-width: 1200px;
+  min-height: 80vh;
+`;
+const Heading = styled.h2`
+  font-size: 30px;
+  text-align: left;
+  margin: 1rem 0;
+`;
